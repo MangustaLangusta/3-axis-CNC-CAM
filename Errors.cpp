e3a5e796647a0f89ddc84ee56bf5c7797d5781b3
MangustaLangusta <Live_While_Alive@mail.ru>
2023-02-17 00:51:27 +1000

    working..

---
+++ Errors.cpp
@@ -0,0 +1,52 @@
+#include "Errors.h"
+
+const std::map<ErrorCode, std::string> Errors::error_codes_string_assignments = {
+	{0x0000, "NORMAL"},
+	{0x0001, "GENERAL_WARNING"},
+	{0x5000, "GENERAL_ERROR"},
+	{0x5001, "ERROR_STL_FILE_INVALID"},
+	{0x5002, "ERROR_STL_FILE_UNEXPECTED_EOF"},
+	{0x5003, "ERROR_STL_FILE_EOF_NOT_REACHED"},
+	{0x5004, "ERROR_FACET_BODY_UNABLE_TO_CREATE"},
+	{0x5005, "ERROR_FACET_BODY_VALIDITY_CHECK_NOT_PASSED"},
+	{0xA000, "GENERAL_FATAL_ERROR"}
+};
+
+std::string Errors::ErrorCodeToString(ErrorCode error_code){
+	std::string result = "";
+	auto it = error_codes_string_assignments.find(error_code);
+	if(it != error_codes_string_assignments.end())
+		result = it->second;
+	return result;
+}
+
+bool Errors::IsFatal(ErrorCode error_code){
+	return (error_code >= GENERAL_FATAL_ERROR);
+}
+
+Error::Error(){}
+
+Error::Error(ErrorCode new_code){
+	code = new_code;
+}
+
+Error::~Error(){}
+
+bool Error::IsFatal(){
+	return 	(code >= GENERAL_FATAL_ERROR);
+}
+
+ErrorsLog::ErrorsLog(){}
+
+ErrorsLog::~ErrorsLog(){}
+
+bool ErrorsLog::HaveErrors(){
+	return !errors.empty();
+}
+std::list<Error> ErrorsLog::GetErrors() const{
+	return errors;
+}
+
+void ErrorsLog::Clear(){
+	errors.clear();
+}
---
+++ Errors.h
@@ -0,0 +1,56 @@
+#ifndef ERROR_CODES_H
+#define ERROR_CODES_H
+
+#include <map>
+#include <string>
+#include <list>
+
+/*			NO ERRORS												*/
+#define NORMAL															0x0000
+/****************************************/
+/*			WARNINGS												*/
+#define GENERAL_WARNING		 									0x0001
+
+/****************************************/
+/*			ERRORS													*/
+#define GENERAL_ERROR																0x5000
+#define ERROR_STL_FILE_INVALID											0x5001
+#define ERROR_STL_FILE_UNEXPECTED_EOF 							0x5002
+#define ERROR_STL_FILE_EOF_NOT_REACHED							0x5003
+#define ERROR_FACET_BODY_UNABLE_TO_CREATE						0x5004
+#define ERROR_FACET_BODY_VALIDITY_CHECK_NOT_PASSED	0x5005
+/****************************************/
+/*			FATAL ERRORS										*/
+#define GENERAL_FATAL_ERROR									0xA000
+
+/****************************************/
+
+using ErrorCode = long;
+
+namespace Errors {
+	extern const std::map<ErrorCode, std::string> error_codes_string_assignments;
+	std::string ErrorCodeToString(ErrorCode error_code);
+	bool IsFatal(ErrorCode error_code);
+};
+
+class Error{
+	public:
+		ErrorCode code;
+		Error();
+		Error(ErrorCode new_code);
+		~Error();
+		bool IsFatal();
+};
+
+class ErrorsLog{
+	private:
+		std::list<Error> errors;
+	public:
+		ErrorsLog();
+		~ErrorsLog();
+		bool HaveErrors();
+		std::list<Error> GetErrors() const;
+		void Clear();
+};
+
+#endif
